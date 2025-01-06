60b40b132b3bcf0ef7ebf47bfde311fff7b28904
anthonydangg <anthonydang33@gmail.com>
2025-01-06 10:53:35 -0800

    created table.sql file

--- src/etl.py
+++ src/etl.py
@@ -4,7 +4,10 @@ import os
 import json
 
 import spotipy
+from spotipy_anon import SpotifyAnon
 from spotipy.oauth2 import SpotifyClientCredentials
+from spotipy.oauth2 import SpotifyOAuth
+
 
 from utils.db import dbconnect
 
@@ -12,34 +15,36 @@ def get_spotify_client():
     load_dotenv('.env')
     client_id = os.getenv("CLIENT_ID")
     client_secret = os.getenv("CLIENT_SECRET")
-    auth_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
-    return spotipy.Spotify(auth_manager=auth_manager)
+    
+    sp = spotipy.Spotify(auth_manager=SpotifyAnon())
 
-def get_top_50_tracks():
+    return sp
+
+def top_50_usa():
     sp = get_spotify_client()
+
     playlist_items = sp.playlist('37i9dQZEVXbLRQDuF5jeBp')['tracks']['items']
 
-    # data = []
-    # for item in playlist_items:
-    #     data.append({
-    #         'item_id': item['track']['id'],
-    #         'song_name': item['track']['name'],
-    #         'artist_id': item['track']['artists'][0]['id'],
-    #         'artist_name': item['track']['artists'][0]['name'],
-    #         'artist_genre': sp.artist(item['track']['artists'][0]['id'])['genres'],
-    #         'track_popularity': item['track']['popularity'],
-    #         'artist_popularity': sp.artist(item['track']['artists'][0]['id'])['popularity']
-    #     })
+    data = []
+    for item in playlist_items:
+        data.append({
+            'item_id': item['track']['id'],
+            'song_name': item['track']['name'],
+            'artist_id': item['track']['artists'][0]['id'], #only takes the first artist. should account for features later
+            'artist_name': item['track']['artists'][0]['name'],
+            'artist_genre': sp.artist(item['track']['artists'][0]['id'])['genres'],
+            'track_popularity': item['track']['popularity'],
+            'artist_popularity': sp.artist(item['track']['artists'][0]['id'])['popularity']
+        })
 
-    # json_data = json.dumps(data)
+    json_data = json.dumps(data)
 
+def insert():
     with dbconnect() as cur:
         insert = '''
                     INSERT INTO test(id)
                     VALUES (%s)'''
-        value = (1,)
+        value = (2,)
         cur.execute(insert, value)
 
-get_top_50_tracks()
-
-#insert Spotify API data into local postgres. proof of concept
+top_50_usa()
